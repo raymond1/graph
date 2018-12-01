ffd67b447078a894820d53bd05ae1818fdf16fd2
Raymond Leon <raymond@Raymonds-MacBook-Pro.local>
2018-12-01 00:43:28 -0500

    I think it's time to use wheels made by other people.

--- server/README.txt
+++ server/README.txt
@@ -1,3 +1,3 @@
-To start server, type in node index
+To start server, type in yarn start.
 
 The server will then start and be accessible at http://localhost:9000
--- server/index.js
+++ server/index.js
@@ -7,29 +7,3 @@ http.createServer((req,res)=>{
     fileServer.serve(req, res);
   }).resume();
 }).listen(9000);
-/*
-var net = require('net');
-
-
-var server = net.createServer();
-
-
-var count = 0;
-var onConnect = function (socket){
-  var socketDataReceivedFunction = function(data){
-    count = count + 1;
-    console.log("count %j\n", count);
-    console.log(data);
-  };
-
-
-  socket.on('data', socketDataReceivedFunction); 
-
-  socket.write("test");
-  socket.end();
-}
-
-server.listen({port: 9000});
-
-server.on('connection', onConnect);
-*/
